c1af2a9f8655d76d854070d30fe48432a114963e
Aman Agrawal <amanagrawal2098@gmail.com>
2020-08-22 19:10:53 +0530

    Automatic end of calling solved+ video calling also improved+token added for phone login

--- app/src/main/java/com/example/chatapp/AllFragments/ChatWork/VideoChat/VideoChatActivity.java
+++ app/src/main/java/com/example/chatapp/AllFragments/ChatWork/VideoChat/VideoChatActivity.java
@@ -13,6 +13,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.chatapp.MainActivity;
 import com.example.chatapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -26,115 +28,88 @@ import com.opentok.android.Session;
 import com.opentok.android.Stream;
 import com.opentok.android.Subscriber;
 
+import java.util.HashMap;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 import pub.devrel.easypermissions.AfterPermissionGranted;
 import pub.devrel.easypermissions.EasyPermissions;
 
 public class VideoChatActivity extends AppCompatActivity implements Session.SessionListener, PublisherKit.PublisherListener {
 
-    private static String API_KEY="46854334",SESSION_ID="1_MX40Njg1NDMzNH5-MTU5NTQ0OTk5NTIzOH5pemd0aFdTVCtDcWtLMWtwN1hNZW84TU1-fg"
-            ,TOKEN="T1==cGFydG5lcl9pZD00Njg1NDMzNCZzaWc9OTE4Yzc5YmRlMWYwMWM0MGNhMTBkYTVlNmRkYTc5NzM2ZWRiZGI1MjpzZXNzaW9uX2lkPTFfTVg0ME5qZzFORE16Tkg1LU1UVTVOVFEwT1RrNU5USXpPSDVwZW1kMGFGZFRWQ3REY1d0TE1XdHdOMWhOWlc4NFRVMS1mZyZjcmVhdGVfdGltZT0xNTk1NDUwMDU0Jm5vbmNlPTAuMjk3NjcyOTk4MTYwMTYyMjQmcm9sZT1wdWJsaXNoZXImZXhwaXJlX3RpbWU9MTU5ODA0MjA1NCZpbml0aWFsX2xheW91dF9jbGFzc19saXN0PQ==";
-    private static final String LOG_TAG=VideoChatActivity.class.getSimpleName();
-    private static final int RC_VIDEO_APP_PERM=124;
+    private static String API_KEY = "46893654", SESSION_ID = "1_MX40Njg5MzY1NH5-MTU5ODEwMjI5ODU5Nn4zY1dyWk9HMzdUVlI1bHFGdlZMSXlkcWh-fg",
+            TOKEN = "T1==cGFydG5lcl9pZD00Njg5MzY1NCZzaWc9MmFmNjk2OGI4NGE1ZTMyM2Q1ZDI2NmQ2ZDFmMmE1MWY0OGUwNTAxYTpzZXNzaW9uX2lkPTFfTVg0ME5qZzVNelkxTkg1LU1UVTVPREV3TWpJNU9EVTVObjR6WTFkeVdrOUhNemRVVmxJMWJIRkdkbFpNU1hsa2NXaC1mZyZjcmVhdGVfdGltZT0xNTk4MTAyNDAwJm5vbmNlPTAuNjE1OTA5ODg5MTc4NzIyOSZyb2xlPXB1Ymxpc2hlciZleHBpcmVfdGltZT0xNjAwNjk0NDAyJmluaXRpYWxfbGF5b3V0X2NsYXNzX2xpc3Q9";
+    private static final String LOG_TAG = VideoChatActivity.class.getSimpleName();
+    private static final int RC_VIDEO_APP_PERM = 124;
     private CircleImageView m_closeVideoChatBtn;
     private DatabaseReference m_usersRef;
-    private String m_userID="",m_SenderUserID="",m_ReceiverUserID="";
+    private String m_userID = "", m_SenderUserID = "", m_ReceiverUserID = "";
     private FrameLayout m_PublisherViewController;
     private FrameLayout m_SubscriberViewController;
     private Session m_Session;
     private Publisher m_Publisher;
     private Subscriber m_Subsriber;
+    private String senderID, callingID = "", ringingID = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_chat);
-        m_usersRef= FirebaseDatabase.getInstance().getReference().child("Users");
-        m_userID=FirebaseAuth.getInstance().getCurrentUser().getUid();
-        m_SenderUserID=getIntent().getExtras().get("senderID").toString();
-        m_ReceiverUserID=getIntent().getExtras().get("receiverID").toString();
+        m_usersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        m_userID = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        m_SenderUserID = getIntent().getExtras().get("senderID").toString();
+        m_ReceiverUserID = getIntent().getExtras().get("receiverID").toString();
 
-        m_closeVideoChatBtn=findViewById(R.id.close_video_chat_btn);
+        m_closeVideoChatBtn = findViewById(R.id.close_video_chat_btn);
         m_closeVideoChatBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                m_usersRef.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        if(snapshot.child(m_userID).hasChild("Ringing")) {
-                            m_usersRef.child(m_userID).child("Ringing").removeValue();
-                            if(m_Publisher!=null)
-                            {
-                                m_Publisher.destroy();
-                            }
-                            if(m_Subsriber!=null)
-                            {
-                                m_Subsriber.destroy();
-                            }
-                            startActivity(new Intent(VideoChatActivity.this, MainActivity.class));
-                            if(m_userID.equals(m_SenderUserID))
-                            {
-                                m_usersRef.child(m_ReceiverUserID).child("Calling").removeValue();
-                                startActivity(new Intent(VideoChatActivity.this, MainActivity.class));
-                            }
-                            else {
-                                m_usersRef.child(m_SenderUserID).child("Calling").removeValue();
-                                startActivity(new Intent(VideoChatActivity.this, MainActivity.class));
-                            }
-                        }
-                        if(snapshot.child(m_userID).hasChild("Calling")) {
-                            m_usersRef.child(m_userID).child("Calling").removeValue();
-                            if(m_Publisher!=null)
-                            {
-                                m_Publisher.destroy();
-                            }
-                            if(m_Subsriber!=null)
-                            {
-                                m_Subsriber.destroy();
-                            }
-                            startActivity(new Intent(VideoChatActivity.this, MainActivity.class));
-                            if(m_userID.equals(m_SenderUserID))
-                            {
-                                m_usersRef.child(m_ReceiverUserID).child("Ringing").removeValue();
-                                startActivity(new Intent(VideoChatActivity.this, MainActivity.class));
-                            }
-                            else {
-                                m_usersRef.child(m_SenderUserID).child("Ringing").removeValue();
-                                startActivity(new Intent(VideoChatActivity.this, MainActivity.class));
-                            }
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-
-                    }
-                });
+                cancelCallingUser();
             }
         });
         requestPermissions();
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+
+        m_usersRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.child(m_ReceiverUserID).child("Dropped").hasChild("dropped")) {
+                    m_usersRef.child(m_ReceiverUserID).child("Dropped").removeValue();
+                    startActivity(new Intent(VideoChatActivity.this, MainActivity.class));
+                }
+                if (snapshot.child(m_userID).child("Dropped").hasChild("dropped")) {
+                    m_usersRef.child(m_userID).child("Dropped").removeValue();
+                    startActivity(new Intent(VideoChatActivity.this, MainActivity.class));
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        EasyPermissions.onRequestPermissionsResult(requestCode,permissions,grantResults,VideoChatActivity.this);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, VideoChatActivity.this);
     }
 
     @AfterPermissionGranted(RC_VIDEO_APP_PERM)
-    private void requestPermissions()
-    {
-        String[] perms={Manifest.permission.INTERNET,Manifest.permission.CAMERA,Manifest.permission.RECORD_AUDIO};
-        if(EasyPermissions.hasPermissions(this,perms))
-        {
-            m_PublisherViewController=findViewById(R.id.publisher_container);
-            m_SubscriberViewController=findViewById(R.id.subsciber_container);
-            m_Session=new Session.Builder(this,API_KEY,SESSION_ID).build();
+    private void requestPermissions() {
+        String[] perms = {Manifest.permission.INTERNET, Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO};
+        if (EasyPermissions.hasPermissions(this, perms)) {
+            m_PublisherViewController = findViewById(R.id.publisher_container);
+            m_SubscriberViewController = findViewById(R.id.subsciber_container);
+            m_Session = new Session.Builder(this, API_KEY, SESSION_ID).build();
             m_Session.setSessionListener(VideoChatActivity.this);
             m_Session.connect(TOKEN);
-        }
-        else {
-            EasyPermissions.requestPermissions(this,"Hey this app requies mic and Camera permissions",RC_VIDEO_APP_PERM,perms);
+        } else {
+            EasyPermissions.requestPermissions(this, "Hey this app requies mic and Camera permissions", RC_VIDEO_APP_PERM, perms);
         }
     }
 
@@ -156,11 +131,10 @@ public class VideoChatActivity extends AppCompatActivity implements Session.Sess
     @Override
     public void onConnected(Session session) {
         Log.i(LOG_TAG, "Session Connected");
-        m_Publisher=new Publisher.Builder(this).build();
+        m_Publisher = new Publisher.Builder(this).build();
         m_Publisher.setPublisherListener(VideoChatActivity.this);
         m_PublisherViewController.addView(m_Publisher.getView());
-        if(m_Publisher.getView() instanceof GLSurfaceView)
-        {
+        if (m_Publisher.getView() instanceof GLSurfaceView) {
             ((GLSurfaceView) m_Publisher.getView()).setZOrderOnTop(true);
         }
         m_Session.publish(m_Publisher);
@@ -174,20 +148,18 @@ public class VideoChatActivity extends AppCompatActivity implements Session.Sess
     @Override
     public void onStreamReceived(Session session, Stream stream) {
         Log.i(LOG_TAG, "Stream Received");
-            if(m_Subsriber==null)
-            {
-                m_Subsriber=new Subscriber.Builder(this,stream).build();
-                m_Session.subscribe(m_Subsriber);
-                m_SubscriberViewController.addView(m_Subsriber.getView());
-            }
+        if (m_Subsriber == null) {
+            m_Subsriber = new Subscriber.Builder(this, stream).build();
+            m_Session.subscribe(m_Subsriber);
+            m_SubscriberViewController.addView(m_Subsriber.getView());
+        }
     }
 
     @Override
     public void onStreamDropped(Session session, Stream stream) {
         Log.i(LOG_TAG, "Stream Dropped");
-        if(m_Subsriber!=null)
-        {
-            m_Subsriber=null;
+        if (m_Subsriber != null) {
+            m_Subsriber = null;
             m_SubscriberViewController.removeAllViews();
         }
     }
@@ -195,5 +167,114 @@ public class VideoChatActivity extends AppCompatActivity implements Session.Sess
     @Override
     public void onError(Session session, OpentokError opentokError) {
         Log.i(LOG_TAG, "Stream Error");
+    }
+
+    private void cancelCallingUser() {
+
+        m_usersRef.child(m_userID)
+                .child("Calling")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists() && snapshot.hasChild("calling")) {
+                            callingID = snapshot.child("calling").getValue().toString();
+                            m_usersRef.child(callingID)
+                                    .child("Ringing")
+                                    .removeValue()
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                m_usersRef.child(m_userID)
+                                                        .child("Calling")
+                                                        .removeValue()
+                                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<Void> task) {
+                                                                if (m_Publisher != null) {
+                                                                    m_Publisher.destroy();
+                                                                }
+                                                                if (m_Subsriber != null) {
+                                                                    m_Subsriber.destroy();
+                                                                }
+                                                                final HashMap<String, Object> callingPickupMap = new HashMap<>();
+                                                                callingPickupMap.put("dropped", "dropped");
+                                                                m_usersRef.child(m_ReceiverUserID).child("Dropped")
+                                                                        .updateChildren(callingPickupMap);
+                                                                startActivity(new Intent(VideoChatActivity.this,
+                                                                        MainActivity.class));
+                                                                finish();
+                                                            }
+                                                        });
+                                            }
+                                        }
+                                    });
+                        } else {
+                            startActivity(new Intent(VideoChatActivity.this,
+                                    MainActivity.class));
+                            finish();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+        //from receiver's side
+
+        m_usersRef.child(m_userID)
+                .child("Ringing")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if (snapshot.exists() && snapshot.hasChild("ringing")) {
+                            ringingID = snapshot.child("ringing").getValue().toString();
+                            m_usersRef.child(ringingID)
+                                    .child("Calling")
+                                    .removeValue()
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                m_usersRef.child(m_userID)
+                                                        .child("Ringing")
+                                                        .removeValue()
+                                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                            @Override
+                                                            public void onComplete(@NonNull Task<Void> task) {
+                                                                if (m_Publisher != null) {
+                                                                    m_Publisher.destroy();
+                                                                }
+                                                                if (m_Subsriber != null) {
+                                                                    m_Subsriber.destroy();
+                                                                }
+                                                                final HashMap<String, Object> callingPickupMap = new HashMap<>();
+                                                                callingPickupMap.put("dropped", "dropped");
+                                                                m_usersRef.child(m_userID).child("Dropped")
+                                                                        .updateChildren(callingPickupMap);
+                                                                startActivity(new Intent(VideoChatActivity.this,
+                                                                        MainActivity.class));
+                                                                finish();
+                                                            }
+                                                        });
+                                            }
+                                        }
+                                    });
+                        } else {
+                            startActivity(new Intent(VideoChatActivity.this,
+                                    MainActivity.class));
+                            finish();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+
     }
 }
--- app/src/main/java/com/example/chatapp/SignupAndLogin/PhoneLoginActivity.java
+++ app/src/main/java/com/example/chatapp/SignupAndLogin/PhoneLoginActivity.java
@@ -21,61 +21,63 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.PhoneAuthCredential;
 import com.google.firebase.auth.PhoneAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.iid.FirebaseInstanceId;
 import com.hbb20.CountryCodePicker;
 
 import java.util.concurrent.TimeUnit;
 
 public class PhoneLoginActivity extends AppCompatActivity {
 
-    private Button m_send_verCodeButton,m_verifyButton;
-    private EditText m_OTP,m_phoneNumber;
+    private Button m_send_verCodeButton, m_verifyButton;
+    private EditText m_OTP, m_phoneNumber;
     private PhoneAuthProvider.OnVerificationStateChangedCallbacks m_Callbacks;
     private String mVerificationId;
     private PhoneAuthProvider.ForceResendingToken mResendToken;
     private FirebaseAuth mAuth;
     private ProgressDialog loadingBar;
     private CountryCodePicker ccp;
-    private String selected_country_code="+91";
+    private DatabaseReference m_UsersRef;
+    private String selected_country_code = "+91";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_login);
-        mAuth=FirebaseAuth.getInstance();
+        mAuth = FirebaseAuth.getInstance();
+        m_UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
         initializeVariables();
         ccp = findViewById(R.id.ccp);
 
-                ccp.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
-                    @Override
-                    public void onCountrySelected() {
-                        selected_country_code = ccp.getSelectedCountryCodeWithPlus();
-                    }
-                });
+        ccp.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
+            @Override
+            public void onCountrySelected() {
+                selected_country_code = ccp.getSelectedCountryCodeWithPlus();
+            }
+        });
         m_send_verCodeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String phoneNumber=selected_country_code+m_phoneNumber.getText().toString();
-                if(TextUtils.isEmpty(phoneNumber))
-                {
+                String phoneNumber = selected_country_code + m_phoneNumber.getText().toString();
+                if (TextUtils.isEmpty(phoneNumber)) {
                     Toast.makeText(PhoneLoginActivity.this, "Enter phone no...", Toast.LENGTH_SHORT).show();
-                }
-                else
-                    {
-                        m_send_verCodeButton.setVisibility(View.INVISIBLE);
-                        m_phoneNumber.setVisibility(View.INVISIBLE);
-                        m_OTP.setVisibility(View.VISIBLE);
-                        m_verifyButton.setVisibility(View.VISIBLE);
-                        loadingBar.setTitle("Phone Verification");
-                        loadingBar.setMessage("Please wait while we are authenticating your phone");
-                        loadingBar.setCanceledOnTouchOutside(false);
-                        loadingBar.show();
-                PhoneAuthProvider.getInstance().verifyPhoneNumber(
-                        phoneNumber,        // Phone number to verify
-                        60,                 // Timeout duration
-                        TimeUnit.SECONDS,   // Unit of timeout
-                        PhoneLoginActivity.this, // Activity (for callback binding)
-                        m_Callbacks);        // OnVerificationStateChangedCallbacks
+                } else {
+                    m_send_verCodeButton.setVisibility(View.INVISIBLE);
+                    m_phoneNumber.setVisibility(View.INVISIBLE);
+                    m_OTP.setVisibility(View.VISIBLE);
+                    m_verifyButton.setVisibility(View.VISIBLE);
+                    loadingBar.setTitle("Phone Verification");
+                    loadingBar.setMessage("Please wait while we are authenticating your phone");
+                    loadingBar.setCanceledOnTouchOutside(false);
+                    loadingBar.show();
+                    PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                            phoneNumber,        // Phone number to verify
+                            60,                 // Timeout duration
+                            TimeUnit.SECONDS,   // Unit of timeout
+                            PhoneLoginActivity.this, // Activity (for callback binding)
+                            m_Callbacks);        // OnVerificationStateChangedCallbacks
 
                 }
             }
@@ -85,13 +87,10 @@ public class PhoneLoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 m_send_verCodeButton.setVisibility(View.INVISIBLE);
                 m_phoneNumber.setVisibility(View.INVISIBLE);
-                String m_verificationCode=m_OTP.getText().toString();
-                if(TextUtils.isEmpty(m_verificationCode))
-                {
+                String m_verificationCode = m_OTP.getText().toString();
+                if (TextUtils.isEmpty(m_verificationCode)) {
                     Toast.makeText(PhoneLoginActivity.this, "Cannot be empty..", Toast.LENGTH_SHORT).show();
-                }
-                else
-                {
+                } else {
                     loadingBar.setTitle("OTP Verification");
                     loadingBar.setMessage("Please wait while we are authenticating your OTP");
                     loadingBar.setCanceledOnTouchOutside(false);
@@ -101,7 +100,7 @@ public class PhoneLoginActivity extends AppCompatActivity {
                 }
             }
         });
-        m_Callbacks=new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+        m_Callbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
             @Override
             public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
                 signInWithPhoneAuthCredential(phoneAuthCredential);
@@ -116,6 +115,7 @@ public class PhoneLoginActivity extends AppCompatActivity {
                 m_OTP.setVisibility(View.INVISIBLE);
                 m_verifyButton.setVisibility(View.INVISIBLE);
             }
+
             @Override
             public void onCodeSent(@NonNull String verificationId,
                                    @NonNull PhoneAuthProvider.ForceResendingToken token) {
@@ -132,23 +132,28 @@ public class PhoneLoginActivity extends AppCompatActivity {
     }
 
     private void initializeVariables() {
-        m_send_verCodeButton=findViewById(R.id.send_ver_code_button);
-        m_verifyButton=findViewById(R.id.verify_button);
-        m_OTP=findViewById(R.id.verification_number_login);
-        m_phoneNumber=findViewById(R.id.phone_number_login);
-        loadingBar=new ProgressDialog(this);
+        m_send_verCodeButton = findViewById(R.id.send_ver_code_button);
+        m_verifyButton = findViewById(R.id.verify_button);
+        m_OTP = findViewById(R.id.verification_number_login);
+        m_phoneNumber = findViewById(R.id.phone_number_login);
+        loadingBar = new ProgressDialog(this);
     }
+
     private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
         mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
+                            String currentUserId = mAuth.getCurrentUser().getUid();
+                            String deviceToken = FirebaseInstanceId.getInstance().getToken();
+                            m_UsersRef.child(currentUserId).child("device_token")
+                                    .setValue(deviceToken);
                             loadingBar.dismiss();
                             Toast.makeText(PhoneLoginActivity.this, "Welcome", Toast.LENGTH_SHORT).show();
                             sendUsertoMainActivity();
                         } else {
-                            String exception=task.getException().toString();
+                            String exception = task.getException().toString();
                             Toast.makeText(PhoneLoginActivity.this, exception, Toast.LENGTH_SHORT).show();
                         }
                     }
@@ -156,7 +161,7 @@ public class PhoneLoginActivity extends AppCompatActivity {
     }
 
     private void sendUsertoMainActivity() {
-        Intent intent=new Intent(PhoneLoginActivity.this, MainActivity.class);
+        Intent intent = new Intent(PhoneLoginActivity.this, MainActivity.class);
         startActivity(intent);
         finish();
     }
